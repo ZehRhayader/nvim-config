d604e6a7ffad17a1bdec293a49b3c2dcf3b54ecf
Sabeel Al Ferdous <zehrhayader@gmail.com>
2026-04-01 23:12:59 +0600

    Added emmet lua

--- init.lua
+++ init.lua
@@ -636,7 +636,7 @@ require('lazy').setup({
         standardjs = {},
         pyright = {},
         emmet_language_server = {
-          filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'scss', 'less', 'svelte', 'vue', 'vue-html' },
+          filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'scss', 'less', 'svelte', 'vue', 'vue-html', 'eruby' },
         },
         html = {},
         tailwindcss = {},
@@ -763,12 +763,10 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
+          },
         },
         opts = {},
       },
